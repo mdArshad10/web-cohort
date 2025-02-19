3203edcca6d54d8c678097f0c3f7c80a5d1349b2
Md Arshad <mdarshd246@gmail.com>
2025-02-19 10:48:57 +0530

    complete the basic logic of review collector of v1

--- week-06/Projects/ReviewCollectors/script.js
+++ week-06/Projects/ReviewCollectors/script.js
@@ -1,1 +1,13 @@
 console.log("this is the Review Collectors");
+
+const reviewForm = document.querySelector("form");
+const reviewLists = document.querySelector("ul");
+
+reviewForm.addEventListener("submit", (ev) => {
+  ev.preventDefault();
+  const userInput = reviewForm[0].value;
+  const createListElement = document.createElement("li");
+  createListElement.innerText = userInput;
+  reviewLists.appendChild(createListElement);
+  reviewForm[0].value = "";
+});
