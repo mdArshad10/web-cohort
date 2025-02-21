dc34084d6c9b156fbc329820f2b1e0c2ece6c50a
Md Arshad <mdarshd246@gmail.com>
2025-02-21 20:00:52 +0530

    complete the challenge-2 of dom's challenge

--- week-06/Assigments/dom/challenge-2/app.js
+++ week-06/Assigments/dom/challenge-2/app.js
@@ -1,3 +1,19 @@
 /**
  * Write your challenge solution here
  */
+console.log("change the heading");
+
+const mainHeading = document.querySelector("#mainHeading");
+const colorBtns = document.querySelectorAll("button");
+
+colorBtns.forEach((item) => {
+  item.addEventListener("click", () => {
+    const innerText = item.innerText;
+
+    if (innerText !== "Reset") {
+      mainHeading.style.color = innerText;
+    } else {
+      mainHeading.style.color = "black";
+    }
+  });
+});
