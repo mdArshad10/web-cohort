94051bd3785ee7c3af3bb08ee24eab0d0fdca443
Md Arshad <mdarshd246@gmail.com>
2025-02-22 12:30:11 +0530

    complete the challenge 9 of dom's challenge

--- week-06/Assigments/dom/second-part/challenge-9/app.js
+++ week-06/Assigments/dom/second-part/challenge-9/app.js
@@ -1,3 +1,27 @@
 /**
  * Write your challenge solution here
  */
+console.log("click on model");
+const toggleBtn = document.querySelector(".toggle-btn");
+const body = document.querySelector("body");
+const modalPanel = document.querySelector(".panel");
+
+body.addEventListener(
+  "click",
+  (event) => {
+    if (modalPanel.classList.contains("active")) {
+      modalPanel.classList.remove(["active"]);
+    } else {
+      toggleBtn.addEventListener("click", (event) => {
+        modalPanel.classList.add(["active"]);
+
+
+        const closeBtn = document.querySelector(".close-btn");
+        closeBtn.addEventListener("click", (event) => {
+          modalPanel.classList.remove(["active"]);
+        });
+      });
+    }
+  },
+  { capture: true }
+);
